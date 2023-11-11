0d02764e23d5b0cdf53516b1a3537fda68793ca9
therealhesham <heshammoha231992@gmail.com>
2023-11-11 12:12:52 +0200

    new commit added

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Registeruser from './users/register';
 
 export default function App() {
+console.log(process.env.EXPO_PUBLIC_URL)
+const Stack =createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Register'>
+<Stack.Screen component={Registeruser} name='Register'/>
+
+</Stack.Navigator>
+    </NavigationContainer>
+ 
   );
 }
 
